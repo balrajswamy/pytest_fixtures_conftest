24a9a5bae7c8e69de9eb438ebf10d88505404ee6
balrajswamy <balrajswamy@gmail.com>
2025-01-04 07:24:34 +0530

    created and tested with fixtures for file level or module level and global level with conftest.py

---
+++ tests/test_checking_validation1.py
@@ -0,0 +1,8 @@
+import pytest
+
+def test_checking_validation1(preWork):
+    print("\nI am checking now from validation1.py!")
+
+def test_checking_validation2(preWork):
+    print("\nI am checking now validation1.py!")
+
---
+++ tests/test_checking_validation2.py
@@ -0,0 +1,8 @@
+import pytest
+
+def test_checking_validation1(preWork):
+    print("\nI am checking now! validation2.py")
+
+def test_checking_validation2(preWork):
+    print("\nI am checking now! validation2.py")
+
